0afeaa5697980d573467a9e0e2a3365415fb0802
Kory Davis <ked130@scarletmail.rutgers.edu>
2017-10-21 13:57:56 -0400

    Step 2 - Shield
    Created the shield object for the Hero. The scipting allows for the
    shield to be constantly rotating about the z axis and dynamically check
    the shield's current level to see if it needs to change the offset to a
    new shield level.

--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -1,50 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Hero : MonoBehaviour {
+	static public Hero S;
 
-	static public Hero		S;
+	[Header ("Set in Inspector")]
+	//these fields control the movement of the ship
+	public float speed = 30;
+	public float rollMult = 45;
+	public float pitchMult = 30;
 
-	public float	speed = 30;
-	public float	rollMult = -45;
-	public float  	pitchMult=30;
-
-	public float	shieldLevel=1;
-
-	public bool	_____________________;
-	public Bounds bounds;
+	[Header ("Set Dynamically")]
+	public float shieldLevel = 1;
 
 	void Awake(){
-		S = this;
-		bounds = Utils.CombineBoundsOfChildren (this.gameObject);
+		if (S == null) {
+			S = this; //set singleton
+		} else {
+			Debug.LogError("Hero.Awake() - Attempted to assign a second Hero.S!");
+		}
 	}
 
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+	void Update(){
+		//Pull in information from the input class
 		float xAxis = Input.GetAxis("Horizontal");
-		float yAxis = Input.GetAxis("Vertical");
+		float yAxis = Input.GetAxis ("Vertical");
+
+		//Change transform.position based on the axes
 
 		Vector3 pos = transform.position;
 		pos.x += xAxis * speed * Time.deltaTime;
 		pos.y += yAxis * speed * Time.deltaTime;
 		transform.position = pos;
-		
-		bounds.center = transform.position;
-		
-		// constrain to screen
-		Vector3 off = Utils.ScreenBoundsCheck(bounds,BoundsTest.onScreen);
-		if (off != Vector3.zero) {  // we need to move ship back on screen
-			pos -= off;
-			transform.position = pos;
-		}
-		
-		// rotate the ship to make it feel more dynamic
-		transform.rotation =Quaternion.Euler(yAxis*pitchMult, xAxis*rollMult,0);
+
+		//rotate the ship to make it feel more dynamic
+		transform.rotation = Quaternion.Euler(yAxis*pitchMult, xAxis*rollMult, 0);
 	}
+
 }
--- Assets/__Scripts/Shield.cs
+++ Assets/__Scripts/Shield.cs
@@ -1,29 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Shield : MonoBehaviour {
-
+	[Header ("Set in Inspector")]
 	public float rotationsPerSecond = 0.1f;
-	public bool ___________________________;
+
+	[Header ("Set Dynamically")]
 	public int levelShown = 0;
 
+	//This non-public variable will not appear in the Inspector
+	Material mat;
 
-	// Use this for initialization
-	void Start () {
-
+	void Start(){
+		mat = GetComponent<Renderer> ().material;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		int currlevel = Mathf.FloorToInt (Hero.S.shieldLevel);
-		if (levelShown != currlevel) {
-			levelShown = currlevel;
-			Material mat = this.GetComponent<Renderer>().material;
-			mat.mainTextureOffset = new Vector2(0.2f*levelShown, 0f);
+
+	void Update (){
+		//Read the current shield level from the Hero Singleton
+		int currLevel = Mathf.FloorToInt(Hero.S.shieldLevel);
+		//if this is different from levelShown...
+		if (levelShown != currLevel) {
+			levelShown = currLevel;
+			//Adjust the texture offset to show different shield level
+			mat.mainTextureOffset = new Vector2( 0.2f*levelShown, 0 );
 		}
-
-		float rZ = (rotationsPerSecond * Time.time * 360) % 360f;
+		//rotate the shield a bit every fram in a time-based way
+		float rZ = -(rotationsPerSecond * Time.time * 3600) % 360f;
 		transform.rotation = Quaternion.Euler (0, 0, rZ);
-
 	}
+
 }
